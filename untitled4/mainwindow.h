38bedfad4efd873bc55c360678126ba96c52f62b
yy.xu <82877636@qq.com>
2024-07-29 17:47:38 +0800

    V1.0.0.240729
    (cherry picked from commit 3a7a82cc5c9ae6eb34c2bf7fc7d7f63181d9d2b3)

--- untitled4/mainwindow.h
+++ untitled4/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+private slots:
+    void onButtonClicked();
+    void onTimerTimeout();
+
 private:
     Ui::MainWindow *ui;
+
+private:
+    QPushButton *button;
+    QTimer      *timer;
 };
 #endif // MAINWINDOW_H
