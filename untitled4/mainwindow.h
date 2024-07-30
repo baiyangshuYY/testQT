5690dc1cf2054dc377700e312f4eb636f93e07e4
yy.xu <82877636@qq.com>
2024-07-30 09:37:15 +0800

    Revert "V1.0.0.240729"
    This reverts commit 38bedfad4efd873bc55c360678126ba96c52f62b.

--- untitled4/mainwindow.h
+++ untitled4/mainwindow.h
@@ -2,8 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
-#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,16 +15,7 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-
-private slots:
-    void onButtonClicked();
-    void onTimerTimeout();
-
 private:
     Ui::MainWindow *ui;
-
-private:
-    QPushButton *button;
-    QTimer      *timer;
 };
 #endif // MAINWINDOW_H
